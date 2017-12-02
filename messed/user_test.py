4fd1c5d825220e721d2535f7a44c6c3aba723394
rkepha <kephaokari@gmail.com>
2017-12-02 12:42:55 +0000

    all methods in the User class passed the unittest

--- messed/user_test.py
+++ messed/user_test.py
@@ -1,61 +1,66 @@
+'''
+import uniittest to create uniittests for User Module
+import User Module to be tested
+import the credential module as well to enable testing of emlemetns that user module imports from this class
+
+'''
 
 import unittest
-from user import User
-from credential import Credential
-
+from user import User # main class to test in this unittest
 
 class TestUser(unittest.TestCase):
-
     def setUp(self):
-        # Create user object
-        self.new_user = User("John","doe")
-
+        self.new_user = User("KEPHA", "TEST")
 
     def tearDown(self):
-        User.user_list = []
+        User.user_list =[]
 
     def test_init(self):
-        self.assertEqual( self.new_user.user_name, "John" )
-        self.assertEqual( self.new_user.user_password, "doe" )
+        self.asserEqual(self.new_user.user_name,"KEPHA")
+        self.assertEqual(self.new_user.user_password,"TEST")
 
     def test_save_user(self):
-        # Saving the new user
         self.new_user.save_user()
-        self.assertEqual( len(User.user_list), 1 )
+        self.assertEqual(len(User.user_list),1)
 
     def test_save_multiple_users(self):
-        # Save the new user
         self.new_user.save_user()
-        test_user = User("Jane","doey")
+        test_user = User("BETTY","BAYO")
         test_user.save_user()
-        self.assertEqual( len(User.user_list), 2)
+        self.assertEqual(len(User.user_list),2)
+
+#    THIS METHODS DEPENDS ON THOSE IMPORTED FORM Credential CLASS TO WORK
 
     def test_find_credential(self):
         # Save the new user
         self.new_user.save_user()
-        test_user = User("Jane","doey")
+        test_user = User("BETTY","BAYO")
         test_user.save_user()
-        found_credential = User.find_credential("Yahoo")
+        found_credential = User.find_credential("KELVIN")
         self.assertEqual( found_credential, False )
 
     def test_log_in(self):
+
         # Save the new user
         self.new_user.save_user()
-        test_user = User("Jane","doey")
+        test_user = User("KEPHA","OKARI")
         test_user.save_user()
-        found_credential = User.log_in("Jane", "doey")
+        found_credential = User.log_in("KEPHA", "OKARI")
         self.assertEqual( found_credential,  Credential.credential_list )
 
     def test_display_user(self):
+
         self.assertEqual( User.display_user() , User.user_list )
 
     def test_user_exist(self):
+
         # Save the new user
         self.new_user.save_user()
-        test_user = User("Jane","doey")
+        test_user = User("JOHN","OKARI")
         test_user.save_user()
+
         # use contact exist method
-        user_exists = User.user_exist("Jane")
+        user_exists = User.user_exist("JOHN")
         self.assertTrue(user_exists)
 
 
