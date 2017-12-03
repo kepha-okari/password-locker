067de0201926c1d311c56d0d9bbb4389f1b55814
rkepha <kephaokari@gmail.com>
2017-12-03 08:51:39 +0000

    created the user shortcodes for navigating the user module and implemented it

--- run.py
+++ run.py
@@ -26,7 +26,7 @@ def save_users(user):
     Function to save a user account
 
     Args:
-        user : the user account to be saved
+        user : the user account to be savedhhhhh
     '''
 
     user.save_user()
@@ -44,29 +44,30 @@ def check_existing_users(name):
 def user_log_in(name, password):
     '''
     Function that allows a user to log into their credential account
-
     Args:
         name : the name the user used to create their user account
+
         password : the password the user used to create their user account
     '''
+
     log_in = User.log_in(name, password)
     if log_in != False:
         return User.log_in(name, password)
 
 def display_users():
     '''
-    Function that returns all the saved users 
+    Function that returns all the saved users
     '''
 
     return User.display_user()
 
 def create_credentail(user_password, name, password):
     '''
-    Function to create a credential 
+    Function to create a credential
 
     Args:
         user_password : the password for Password Locker
-        name : the name of the account 
+        name : the name of the account
         password : the password for the account
     '''
 
@@ -103,7 +104,7 @@ def display_credentials(password):
 
 def create_generated_password(name):
     '''
-    Function that generates a password for the user 
+    Function that generates a password for the user
 
     Args:
         name : the name of the account
@@ -117,19 +118,18 @@ def main():
     Function running the Password Locker app
     '''
 
-    print('''Welcome to Password Locker \n
-Use these short codes to get around''')
+    print('''WELCOME TO PASSWORD LOCKER!!! \n ''')
 
     while True:
         '''
         Loop that is running the entire application
         '''
 
-        print('''   Short codes:
-        cu - create a Password Locker account \n
-        du - display names of current Password Locker users \n
-        lg - log into your Password Locker account \n
-        ex - exit the Password Locker account''')
+        print('''SHORT CODES: \n
+        (cu) - CREATE PASSWORD LOCKER ACCOUNT \n
+        (du) - DISPLAY USERS OF PASSWORD LOCKER \n
+        (lg) - LOG INTO YOUR PASSWORD LOCKER ACOUNT \n
+        (ex) - EXIT PASSWORD LOCKER ACCOUNT''')
 
         # Get short codes from the user
         short_code = input().lower()
@@ -158,7 +158,7 @@ Use these short codes to get around''')
 
         elif short_code == 'du':
             '''
-            Display the names of the current users 
+            Display the names of the current users
             '''
 
             if display_users():
@@ -299,6 +299,3 @@ Use these short codes to get around''')
 
 if __name__ == '__main__':
     main()
-
-
-
