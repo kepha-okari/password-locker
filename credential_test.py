3ef7e19f6e8461e3af42a3ec339c7958e193e010
rkepha <kephaokari@gmail.com>
2017-12-03 23:51:35 +0300

    added docstrings and comments for the credential module

--- credential_test.py
+++ credential_test.py
@@ -13,24 +13,40 @@ class TestCredential(unittest.TestCase):
         unittest.TestCase : Test case class that helps create test cases
     '''
     def setUp(self):
+        '''
+        Set up method to run before each test case
+        '''
         # Create credential object
         self.new_credential = Credential("doe","Yahoo","yahoo17")
 
     def tearDown(self):
+        '''
+            tears down / cleans up data for every variable after the case tests are
+         over for each method
+        '''
         Credential.credential_list = []
 
     def test_init(self):
+        '''
+            checks if the initialization of varaibles are handled properly
+        '''
         self.assertEqual( self.new_credential.user_password, "doe")
         self.assertEqual( self.new_credential.credential_name, "Yahoo" )
         self.assertEqual( self.new_credential.credential_password, "yahoo17" )
 
     def test_save_credential(self):
+        '''
+            test case to ensyre credentials are saved
+        '''
         # Save the new credential
         self.new_credential.save_credential()
 
         self.assertEqual( len(Credential.credential_list), 1 )
 
     def test_save_multiple_credentials(self):
+        '''
+            test case to ensure multiple users are saved
+        '''
         # Save the new credential
         self.new_credential.save_credential()
 
@@ -41,12 +57,17 @@ class TestCredential(unittest.TestCase):
         self.assertEqual( len(Credential.credential_list), 2)
 
     def test_generate_password(self):
+        '''
+            test case to ensure random passwords are generated for credentials
+        '''
         generated_password = self.new_credential.generate_password()
 
         self.assertEqual( len(generated_password), 8 )
 
     def test_display_credential(self):
-
+        '''
+        test case for display credential method
+        '''
         # Save the new credential
         self.new_credential.save_credential()
 
@@ -61,6 +82,9 @@ class TestCredential(unittest.TestCase):
         self.assertEqual( len(Credential.display_credential("doe2")) , 2 )
 
     def test_credential_exist(self):
+        '''
+            test case to ensure credentials in question exist
+        '''
         # Save the new credential
         self.new_credential.save_credential()
 
